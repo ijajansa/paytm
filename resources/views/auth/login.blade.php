44a4d809a39a939727a536a018c003821280e2a9
IjajAnsa <ijajansa19@gmail.com>
2023-12-05 09:51:43 +0530

    info

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
   <!----======== CSS ======== -->
   <link rel="stylesheet" href="{{asset('style2.css')}}">
   <!----===== Iconscout CSS ===== -->
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
@@ -48,6 +48,25 @@
       .container form {
         height: 100% !important;
       }
+      body{
+        display: block !important;
+      }
+
+      .container {
+    max-width: 100%;
+    width: 100%;
+    border-radius: 6px;
+    padding: 30px;
+     margin: 0 0; 
+    background-color: #fff;
+    box-shadow: 0 5px 10px rgba(0,0,0,0.1);
+}
+.con2{
+    margin-top: 10px;
+}
+    }
+    ol li {
+      font-size: 13px;
     }
 
   </style>
@@ -88,9 +107,9 @@
 
       <div class="details ID">
         <p align="center" style="margin:0;padding: 0" class="w3-animate-right">
-        <button type="submit" class="nextBtn " style="margin-bottom: 2px !important">
-          <span class="btnText">Login</span>
-        </button>
+          <button type="submit" class="nextBtn " style="margin-bottom: 2px !important">
+            <span class="btnText">Login</span>
+          </button>
         </p>
       </div> 
       <p align="center" style="margin-top: 10px;">or</p>
@@ -102,7 +121,24 @@
 
   </form>
 </div>
+<div class="container con2">
+  <h5 style="color: green;">* Dear Agents Please note below points.</h5>
+  <ol>
+    <li>सफलतापूर्वक पंजीकृत होने के बाद सभी एजेंटों को ग्राहक से दिए गए  QR Code पर 1 रुपये का आउटगोइंग  लेनदेन करना है। (जो भुगतान के लिए पात्र हैं)</li>
 
+    <li>Status COMPLETED और  Status2 ACTIVE दर्शाती है कि यूपीआई बनाया गया है और दिए गए  QR Code पर एक आउटगोइंग लेनदेन भी किया गया है। (जो भुगतान के लिए पात्र हैं)
+    </li>
+    <li>User Type में  NEW_USER और  DORMANT_USER दोनों  UPI निर्माण और दिए  गए  QR Code पर एक आउटगोइंग लेनदेन के बाद संबंधित (जो भुगतान के लिए पात्र हैं)
+    </li>
+    <li>Status ACTIVE या - और Status2 जैसा कि HANDLE_CREATION दर्शाता है कि UPI बनाया गया है, हालांकि दिए गए QR Code पर एक आउटगोइंग लेनदेन नहीं हुआ है। (जो भुगतान के लिए पात्र नहीं हैं)
+    </li>
+    <li>Status & status2 में FAILURE बताती है कि ग्राहक पहले से ही Paytm पर है और सक्रिय है। (जो भुगतान के लिए पात्र नहीं हैं)</li>
+
+    <li>स्टेटस Overridden का मतलब है कि ग्राहक पहले ही अन्य लिंक के साथ यूपीआई का प्रयास कर चुके हैं। (जो भुगतान के लिए पात्र नहीं हैं)</li>
+
+    <li>Referee id जो  PayTm की आंतरिक अद्वितीय उपयोगकर्ता संदर्भ आईडी है।</li>
+  </ol>
+</div>
 <script src="{{asset('script.js')}}"></script>
 </body>
 </html>
